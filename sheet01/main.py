fd23fd3a24e3854dbdaf4086c5e81b8f0bcd7dce
Marten Ringwelski <git@maringuu.de>
2022-10-17 19:47:30 +0200

    sheet01: Solve task 1

--- sheet01/main.py
+++ sheet01/main.py
@@ -1,13 +1,30 @@
-import igraph
+import igraph as ig
+import numpy as np
+
 
 def task1():
-    graph = igraph.Graph.Read_Pickle('data/ogbn-arxiv.pickle')
+    graph = ig.Graph.Read_Pickle('data/ogbn-arxiv.pickle')
+    # TODO account for the fact that multiple papers can have the same amount of citations
+    citations = graph.degree(graph.vs, mode="in")
+    most_citations_vertex = np.argsort(citations)[-1]
+    print(f"The paper with the most citations is in subject class: {graph.vs['label'][most_citations_vertex]}")
+
+    # Now we get the subject of the papers citing the above paper
+    papers = graph.incident(most_citations_vertex, mode="in")
+    # TODO this is wrong since Graph.incident only returns the edges but not the vertecies. Maybe use EdgeSeq.target
+    subjects = graph.vs[papers]["label"]
+    # TODO account for tha fact that multiple subjects can have the same amount
+    most_popular_subject = np.argsort(np.bincount(subjects))[-1]
+    print(f"The subject area that most papers that cite the above paper belong to is {most_popular_subject}")
+
 
 def task2():
     pass
 
+
 def task3():
     pass
 
+
 def main():
-    pass
+    task1()
