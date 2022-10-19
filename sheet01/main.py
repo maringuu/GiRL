05e1cfabfe281007d0ed5efaf3f11bb5fb0969a1
Marten Ringwelski <git@maringuu.de>
2022-10-19 13:07:06 +0200

    sheet01: Fix solution of task 1

--- sheet01/main.py
+++ sheet01/main.py
@@ -3,19 +3,27 @@ import numpy as np
 
 
 def task1():
-    graph = ig.Graph.Read_Pickle('data/ogbn-arxiv.pickle')
-    # TODO account for the fact that multiple papers can have the same amount of citations
-    citations = graph.degree(graph.vs, mode="in")
-    most_citations_vertex = np.argsort(citations)[-1]
-    print(f"The paper with the most citations is in subject class: {graph.vs['label'][most_citations_vertex]}")
+    print(f"Vertex count: {len(graph.vs)}")
+    print(f"Edge count: {len(graph.es)}")
+
+    citations = np.array(graph.degree(graph.vs, mode="in"))
+    most_citation_count = np.max(citations)
+    # FIXME this should probably use np.indices instead of np.arange
+    most_citation_indices = np.arange(*citations.shape)[citations == most_citation_count]
+    print(f"The papers with the most citations are in subject classes: {graph.vs[most_citation_indices]['label']}")
+    print(f"They have {most_citation_count} citations")
 
     # Now we get the subject of the papers citing the above paper
-    papers = graph.incident(most_citations_vertex, mode="in")
-    # TODO this is wrong since Graph.incident only returns the edges but not the vertecies. Maybe use EdgeSeq.target
-    subjects = graph.vs[papers]["label"]
+    _papers_edges = np.array([graph.incident(v, mode="in") for v in most_citation_indices]).flatten()
+    papers_edges = graph.es.select(_papers_edges)
+    # The source is the paper that is citing the target
+    papers = np.array([edge.source for edge in papers_edges])
+    subjects = np.array(graph.vs[papers]["label"]).squeeze()
     # TODO account for tha fact that multiple subjects can have the same amount
-    most_popular_subject = np.argsort(np.bincount(subjects))[-1]
-    print(f"The subject area that most papers that cite the above paper belong to is {most_popular_subject}")
+    binned_subjects = np.bincount(subjects)
+    most_popular_subject_count = np.max(binned_subjects)
+    most_popular_subjects = np.arange(*binned_subjects.shape)[binned_subjects == most_popular_subject_count]
+    print(f"The subject area that most papers that cite the above paper belong to is {most_popular_subjects}")
 
 
 def task2():
